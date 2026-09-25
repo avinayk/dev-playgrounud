8b4faedbe2a644edb29b1ff1433757d936234bb8
avinayk <avinaykumar.web@gmail.com>
2026-09-25 17:16:11 +0530

    build
    build

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -125,7 +125,7 @@ app.use((err: Error, req: Request, res: Response, next: any) => {
    ═══════════════════════════════════════════ */
 const io = new SocketIOServer(server, {
   cors: {
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: ['http://localhost:5173', 'http://localhost:3000','http://localhost:5173/dev', 'http://localhost:3000/dev','https://www.playgroundleague.pro/dev/api','https://www.playgroundleague.pro/dev'],
     methods: ['GET', 'POST'],
     credentials: true,
   },
